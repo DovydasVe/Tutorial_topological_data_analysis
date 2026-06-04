dcfed3682599919087726779fca27bf8c1483faf
Dovydas <dovydas.venc@gmail.com>
2026-06-04 21:11:13 +0200

    Implement rips and nerve complexes and visualised results in a 2D dataset

--- simplicial_complex.py
+++ simplicial_complex.py
@@ -1,7 +1,12 @@
 from itertools import combinations
+import numpy as np
+from scipy.spatial.distance import cdist
 
-TESTING_S = True
-TESTING_SC = True
+# S - simplex, SC - simplicial complex, C - specific complexes
+TESTING_S = False
+TESTING_SC = False
+TESTING_C = True
+
 
 class Simplex:
     """
@@ -86,8 +91,51 @@ class SimplicialComplex:
         return skeleton_set
     
     def __repr__(self):
-        return f"{self.vertices}"
+        return f"{self.simplices}"
+    
+    def rips_complex(self, points, eps):
+        """ Uses Euclidean norm """
+        distance_matrix = cdist(points, points, metric='euclidean')
+        adjacency_matrix = (distance_matrix <= eps).astype(int)
+        np.fill_diagonal(adjacency_matrix, 0)
+        n = len(points)
 
+        edges = []
+        for i, j in combinations(range(n), 2):
+            if adjacency_matrix[i, j]:
+                edges.append(Simplex((i, j)))
+        triangles = []
+        for i, j, k in combinations(range(n), 3):
+            if adjacency_matrix[i, j] and adjacency_matrix[i, k] and adjacency_matrix[j, k]:
+                triangles.append(Simplex((i, j, k)))
+        tetras = []
+        for i, j, k, l in combinations(range(n), 4):
+            if (adjacency_matrix[i, j] and adjacency_matrix[i, k] and adjacency_matrix[i, l] and
+                adjacency_matrix[j, k] and adjacency_matrix[j, l] and adjacency_matrix[k, l]):
+                tetras.append(Simplex((i, j, k, l)))
+
+        self.add_simplices(edges + triangles + tetras)
+        return self
+    
+    def nerve_complex(self, sets):
+        n = len(sets)
+
+        edges = []
+        for i, j in combinations(range(n), 2):
+            if sets[i] & sets[j]:
+                edges.append(Simplex((i, j)))
+        triangles = []
+        for i, j, k in combinations(range(n), 3):
+            if sets[i] & sets[j] & sets[k]:
+                triangles.append(Simplex((i, j, k)))
+        tetras = []
+        for i, j, k, l in combinations(range(n), 4):
+            if sets[i] & sets[j] & sets[k] & sets[l]:
+                tetras.append(Simplex((i, j, k, l)))
+
+        self.add_simplices(edges + triangles + tetras)
+        return self
+        
 
 if __name__ == "__main__":
     if TESTING_S:
@@ -114,6 +162,20 @@ if __name__ == "__main__":
         print(my_sc.dim())
         print(my_sc.is_valid())
         print(my_sc.skeleton(1))
+
+
+    if TESTING_C:
+        complex = SimplicialComplex()
+        rips = complex.rips_complex([(0,0), (1,0), (0,1), (1,1)], 2)
+        print(rips.by_dim)
+        print(rips.is_valid())
+        complex = SimplicialComplex()
+        nerve = complex.nerve_complex([{0,1}, {1,2}, {2,3}, {3,0}])
+        print(nerve.by_dim)
+        print(nerve.is_valid())
+
+
+
 
 
         
